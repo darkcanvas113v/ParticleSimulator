2d8166e328cada1e7a571bcd518b13ba5d40d40d
Vasilev Darkhan <darkcanvas113v@gmail.com>
2022-05-04 21:33:49 +0900

    Исправил перегрузку операторов для Vector2.

--- libs/rtmath/include/rtmath.h
+++ libs/rtmath/include/rtmath.h
@@ -3,15 +3,22 @@
 struct Vector2 {
     float x, y;
 
-    Vector2 operator+(const Vector2& vec);
-    Vector2 operator-(const Vector2& vec);
-    Vector2 operator*(float n);
+    // Vector2 operator+(const Vector2& v1, const Vector2& v2);
+    // Vector2 operator-(const Vector2& v1, const Vector2& v2);
+    // Vector2 operator-(const Vector2& vec);
+    // Vector2 operator*(float n);
     void operator=(const Vector2&);
 
     float distSqrd(const Vector2& vec);
+    float dist(const Vector2& vec);
     float lengthSqrd();
     float length();
     Vector2 normalized();
 
     float dot(const Vector2& vec);
 };
+
+Vector2 operator+(const Vector2& v1, const Vector2& v2);
+Vector2 operator-(const Vector2& v1, const Vector2& v2);
+Vector2 operator*(const Vector2& v1, float n);
+Vector2 operator*(float n, const Vector2& v1);
--- libs/rtmath/src/Vector2.cpp
+++ libs/rtmath/src/Vector2.cpp
@@ -1,16 +1,20 @@
 #include "../include/rtmath.h"
 #include <math.h>
 
-Vector2 Vector2::operator+(const Vector2& vec) {
-  return Vector2 {x + vec.x, y + vec.y};
+Vector2 operator+(const Vector2& v1, const Vector2& v2) {
+  return Vector2 {v1.x + v2.x, v1.y + v2.y};
 }
 
-Vector2 Vector2::operator-(const Vector2& vec) {
-  return Vector2 {x - vec.x, y - vec.y};
+Vector2 operator-(const Vector2& v1, const Vector2& v2) {
+  return Vector2 {v1.x - v2.x, v1.y - v2.y};
 }
 
-Vector2 Vector2::operator*(float n) {
-  return Vector2 {x * n, y * n};
+Vector2 operator*(const Vector2& v1, float n) {
+  return Vector2 {v1.x * n, v1.y * n};
+}
+
+Vector2 operator*(float n, const Vector2& v1) {
+  return Vector2 {v1.x * n, v1.y * n};
 }
 
 void Vector2::operator=(const Vector2& vec) {
@@ -20,6 +24,10 @@ void Vector2::operator=(const Vector2& vec) {
 
 float Vector2::distSqrd(const Vector2& vec) {
   return (x - vec.x) * (x - vec.x) + (y - vec.y) * (y - vec.y);
+}
+
+float Vector2::dist(const Vector2& vec) {
+  return sqrt(distSqrd(vec));
 }
 
 float Vector2::lengthSqrd() {
